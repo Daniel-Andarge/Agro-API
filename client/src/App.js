7c44fd3575551599f0673deaeb598905770d37ee
Daniel-Andarge <andargedaniel90@gmail.com>
2023-04-20 17:30:48 +0300

    client

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
 import './App.css';
 
 import Home from './components/Home';
@@ -18,7 +18,7 @@ function App() {
         {/* Add Menu Component */}
         <Navbarmenu />
         
-        <Routes> 
+        <Switch> 
           <Route exact path="/" component={Home}/>
           <Route path="/About" component={About}/>
           <Route path="/Online" component={Online}/>
@@ -26,7 +26,7 @@ function App() {
           <Route path="/Contact" component={Contact}/>
 
           
-        </Routes>
+        </Switch>
       </Router>
 
     </div>
